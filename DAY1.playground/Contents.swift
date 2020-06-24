a46793015dbb73852ada6ad5b41e65e9c3f99339
SANDEEP BANDARU <SandeepBandaru@SANDEEPs-MacBook-Pro-2.local>
2020-06-25 00:06:23 +0530

    Day1

---
+++ DAY1.playground/Contents.swift
@@ -0,0 +1,61 @@
+//: A UIKit based Playground for presenting user interface
+// Day1 
+import UIKit
+
+//Variables
+
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+
+//Strings and Integers
+
+var age = 38
+var population = 8_000_000
+
+//Multi-line Strings
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+//Doubles and Booleans
+
+var pi = 3.141
+var awesome = true
+var not_awesome = false
+
+var myInt = 1
+var myDouble = 1.0
+
+
+//String Interpolation
+
+var score = 85
+var str3 = "Your score was \(score)"
+
+
+var results = "The test results are here: \(str3)"
+
+//Constants
+
+let taylor = "swift"
+
+let str4 = "Hello, playground"
+
+
+//Type annotaions
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
