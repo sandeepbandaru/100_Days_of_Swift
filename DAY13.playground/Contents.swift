29bf3d2f871cae6dcf4a9c61e702bc0401ad4ae9
sandeepbandaru <bandarusandeep225@gmail.com>
2020-07-05 23:33:01 +0530

    DAY13

---
+++ DAY13.playground/Contents.swift
@@ -0,0 +1,15 @@
+//DAY13
+// review - DAY 1 to 4
+/*
+ Variables and constants
+ Types of Data
+ Operators
+ String interpolation
+ Arrays
+ Dictionaries
+ Conditional statements
+ Loops
+ Switch case
+ */
+
+
