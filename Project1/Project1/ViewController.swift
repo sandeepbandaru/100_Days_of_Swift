e385c637ea7efe8c283c57e5ef975a7588d5cf57
sandeepbandaru <bandarusandeep225@gmail.com>
2020-07-08 20:03:58 +0530

    DAY16

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by SANDEEP BANDARU on 08/07/20.
+//  Copyright © 2020 SANDEEP BANDARU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+}
+
