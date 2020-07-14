8bf7f44942f747872c91e50f8764b3a1fdf9f142
sandeepbandaru <bandarusandeep225@gmail.com>
2020-07-14 23:55:32 +0530

    DAY23

--- DAY23challenge/DAY23challenge/ViewController.swift
+++ DAY23challenge/DAY23challenge/ViewController.swift
@@ -1,59 +1,59 @@
 //
 //  ViewController.swift
-//  Project1
+//  DAY23challenge
 //
-//  Created by SANDEEP BANDARU on 08/07/20.
+//  Created by SANDEEP BANDARU on 14/07/20.
 //  Copyright © 2020 SANDEEP BANDARU. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UITableViewController {
-    var pictures = [String]()
-    
+    var flags = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        title = "Strom Viewer"
-         navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Flags Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
         
         for item in items {
-            if item.hasSuffix("jpg"){
-                pictures = pictures.sorted()
-                pictures.append(item)
+            if item.hasSuffix("png") {
+                flags = flags.sorted()
+                flags.append(item)
             }
         }
-        print(pictures)
+        print(flags)
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pictures.count
+        return flags.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = flags[indexPath.row]
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
-            vc.selctedImage = pictures[indexPath.row]
-            //print(vc.selctedImage)
-            vc.totalPictures = pictures.count
-            //print(vc.totalPictures)
-            vc.selectedPictureNumber = indexPath.row + 1
-            //print(vc.selectedPictureNumber)
+            vc.selectedImage = flags[indexPath.row]
+            
+            vc.totalFlags = flags.count
+        
+            vc.selectedFlagNumber = indexPath.row + 1
+        
             navigationController?.pushViewController(vc, animated: true)
         }
         else {
             print("error")
         }
     }
+
 }
 
