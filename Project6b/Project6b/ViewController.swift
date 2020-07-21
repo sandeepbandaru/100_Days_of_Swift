eb3e3afcc069c6e8985e9810a7d42faed026bb13
sandeepbandaru <bandarusandeep225@gmail.com>
2020-07-21 23:55:13 +0530

    DAY30

---
+++ Project6b/Project6b/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by SANDEEP BANDARU on 21/07/20.
+//  Copyright © 2020 SANDEEP BANDARU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
