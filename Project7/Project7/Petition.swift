7607cbe724ba2698b3e2d2a64a3943abd354edb6
sandeepbandaru <bandarusandeep225@gmail.com>
2020-07-23 23:48:48 +0530

    DAY33

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by SANDEEP BANDARU on 23/07/20.
+//  Copyright © 2020 SANDEEP BANDARU. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title : String
+    var body : String
+    var signatureCount : Int
+    
+}
---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by SANDEEP BANDARU on 23/07/20.
+//  Copyright © 2020 SANDEEP BANDARU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var petitions = [Petition]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+                return
+            }
+        }
+    }
+    
+    func parse(json: Data){
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath)
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+}
+
